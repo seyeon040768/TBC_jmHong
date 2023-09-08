2e91509a5050ba2535862fcefe980237d67705dc
seyeon040768 <seyeon040768@gmail.com>
2023-09-08 23:42:05 +0900

    update(code): Section7 Lecture1

---
+++ code/Section7/Lecture1/Lecture1.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int number;
+
+	printf("Input a positive integer: ");
+	scanf("%d", &number);
+
+	if (number % 2 == 0)
+	{
+		printf("Even");
+	}
+	else
+	{
+		printf("Odd");
+	}
+
+	return 0;
+}
