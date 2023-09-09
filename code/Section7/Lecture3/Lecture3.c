2d8b8d667efdefc4752bc62f276230ee69c6356e
seyeon040768 <seyeon040768@gmail.com>
2023-09-10 01:21:16 +0900

    update(code): Section7 Lecture3, 4 and modify Lecture 1, 2

---
+++ code/Section7/Lecture3/Lecture3.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char ch;
+
+	while ((ch = getchar()) != '\n')
+	{
+		if (isdigit(ch))
+		{
+			ch = '*';
+		}
+
+		if (islower(ch))
+		{
+			ch = toupper(ch);
+		}
+		else if (isupper(ch))
+		{
+			ch = tolower(ch);
+		}
+
+		putchar(ch);
+	}
+
+	return 0;
+}
