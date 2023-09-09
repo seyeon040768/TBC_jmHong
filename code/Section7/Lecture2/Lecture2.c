2b0f512259335b4969768be23fdff57ca8dccd45
seyeon040768 <seyeon040768@gmail.com>
2023-09-10 00:20:17 +0900

    update(code): Section7 Lecture2

---
+++ code/Section7/Lecture2/Lecture2.c
@@ -0,0 +1,36 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	/*
+	* 1. Introduce getchar(), putchar()
+	* 2. Use while loop to process character sequences
+	* 3. Filter a specific character
+	* 4. Convert numbers to asterisks
+	* 5. Lower characters to Upper characters
+	*/
+
+	char ch;
+
+	while ((ch = getchar()) != '\n')
+	{
+		/*if (ch == 'f' || ch == 'F')
+		{
+			ch = ' ';
+		}*/
+
+		/*if (ch >= '0' && ch <= '9')
+		{
+			ch = '*';
+		}*/
+
+		/*if (ch >= 'a' && ch <= 'z')
+		{
+			ch -= 'a' - 'A';
+		}*/
+
+		putchar(ch);
+	}
+
+	return 0;
+}
