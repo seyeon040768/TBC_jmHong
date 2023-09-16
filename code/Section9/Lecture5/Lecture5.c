02d4833dca1ac32eb62d1070a6615530b9066d51
seyeon040768 <seyeon040768@gmail.com>
2023-09-16 22:39:16 +0900

    update(code, image): Section9 Lecture5

---
+++ code/Section9/Lecture5/Lecture5.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int int_max(int i, int j)
+{
+	int m;
+	m = (i > j) ? i : j;
+	return m;
+}
+
+int main(void)
+{
+	int a; // 1
+
+	a = int_max(1, 2); // 2
+	// 3
+	{
+		int b; // 4
+		b = int_max(4, 5); // 5
+		// 6
+		{
+			int b = 123; // 7
+		}
+		// 8
+		{
+			int c = 456; // 9
+		}
+		// 10
+	}
+	// 11
+	return 0;
+}
