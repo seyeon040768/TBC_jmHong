57eb0d8034c9cf4b32cfc56ef5eb292c88afd452
seyeon040768 <seyeon040768@gmail.com>
2023-09-16 23:28:07 +0900

    update(code): Section9 Lecture6

---
+++ code/Section9/Lecture6/Lecture6.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+void MyFunc(int);
+
+int main(void)
+{
+	MyFunc(1);
+
+	return 0;
+}
+
+void MyFunc(int n)
+{
+	printf("Level %d, address of variable n = %p\n", n, &n);
+
+	if (n < 4)
+	{
+		MyFunc(n + 1);
+	}
+
+	printf("Level %d, address of variable n = %p\n", n, &n);
+}
