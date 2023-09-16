fb95c7a3a5cb4165165ea9e0ea689310252e2ccc
seyeon040768 <seyeon040768@gmail.com>
2023-09-16 17:22:43 +0900

    update(code): Section9 Lecture1

---
+++ code/Section9/Lecture1/Lecture1.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define WIDTH 20
+#define NAME "Seyeon Lee"
+#define ADDRESS "Seoul, Korea"
+
+void PrintMultipleChars(char c, int num, bool printNewline)
+{
+	for (int i = 0; i < num; ++i)
+	{
+		printf("%c", c);
+	}
+
+	if (printNewline)
+	{
+		printf("\n");
+	}
+}
+
+void PrintCenteredStr(char str[])
+{
+	int nBlanks;
+
+	nBlanks = (WIDTH - strlen(str)) / 2;
+	PrintMultipleChars(' ', nBlanks, false);
+
+	printf("%s\n", str);
+}
+
+int main(void)
+{
+	int nBlanks = 0;
+
+	PrintMultipleChars('*', WIDTH, true);
+
+	PrintCenteredStr(NAME);
+	PrintCenteredStr(ADDRESS);
+	
+	PrintMultipleChars('*', WIDTH, true);
+
+	return 0;
+}
