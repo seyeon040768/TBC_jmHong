3666e1415ad473468f9f0a6b8248c4567844759d
seyeon040768 <seyeon040768@gmail.com>
2023-09-16 21:37:52 +0900

    update(code): Section9 Lecture4

---
+++ code/Section9/Lecture4/Lecture4.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int int_max(int i, int j)
+{
+	int m;
+	m = (i > j) ? i : j;
+	return m;
+}
+
+int main(void)
+{
+	int a;
+
+	a = int_max(1, 2);
+
+	printf("%d\n", a); // 2
+	printf("%p\n", &a); // 0000001BA6AFFBD4
+
+	{
+		int a;
+		a = int_max(4, 5);
+
+		printf("%d\n", a); // 5
+		printf("%p\n", &a); // 0000001BA6AFFBF4
+
+		int b = 123;
+	}
+
+	printf("%d\n", a); // 2
+	printf("%p\n", &a); // 0000001BA6AFFBD4
+
+	return 0;
+}
