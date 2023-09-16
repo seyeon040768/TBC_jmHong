369b0b9492c1881affd9f5ac545b3950606764c0
seyeon040768 <seyeon040768@gmail.com>
2023-09-17 00:16:56 +0900

    update(code): Section9 Lecture8

---
+++ code/Section9/Lecture8/Lecture8.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+long LoopFactorial(int n);
+long RecursiveFactorial(int n);
+
+int main(void)
+{
+	int num = 5;
+
+	printf("%d\n", LoopFactorial(num));
+	printf("%d\n", RecursiveFactorial(num));
+
+	return 0;
+}
+
+long LoopFactorial(int n)
+{
+	long ret = 1;
+
+	while (n > 1)
+	{
+		ret *= n;
+		--n;
+	}
+
+	return ret;
+}
+
+long RecursiveFactorial(int n)
+{
+	if (n <= 1)
+	{
+		return 1;
+	}
+
+	return n * RecursiveFactorial(n - 1);
+}
