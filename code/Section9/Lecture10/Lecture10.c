4868d57cca5eaa0b06223114e1a6df4984e19d35
seyeon040768 <seyeon040768@gmail.com>
2023-09-17 14:44:16 +0900

    update(code): Section9 Lecture10

---
+++ code/Section9/Lecture10/Lecture10.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int Fibonacci(int number);
+
+int main(void)
+{
+	for (int count = 1; count < 13; ++count)
+	{
+		printf("%d ", Fibonacci(count));
+	}
+
+	return 0;
+}
+
+int Fibonacci(int number)
+{
+	if (number <= 2)
+	{
+		return 1;
+	}
+
+	return Fibonacci(number - 1) + Fibonacci(number - 2);
+}
