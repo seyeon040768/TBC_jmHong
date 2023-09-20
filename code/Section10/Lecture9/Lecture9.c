e34b03e1c859357e0fefe32e2b4d9e7a7819b3f7
Seyeon Lee <seyeon040768@gmail.com>
2023-09-21 02:48:31 +0900

    update(code): Section10 Lecture9

---
+++ code/Section10/Lecture9/Lecture9.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5] = { 100, 200, 300, 400, 500 };
+	int* ptr1, * ptr2, * ptr3;
+
+	ptr1 = arr; // Assignment
+
+	printf("%p %d %p\n", ptr1, *ptr1, &ptr1); // dereferencing
+
+	ptr2 = &arr[2]; // Address-of operator &
+
+	printf("%p %d %p\n", ptr2, *ptr2, &ptr2);
+
+	ptr3 = ptr1 + 4; // Adding an integer from a pointer
+
+	printf("%p %d %p\n", ptr3, *ptr3, &ptr3);
+
+	// Differencing
+	printf("%td\n", ptr3 - ptr1); // t is for pointer difference
+
+	ptr3 -= 4;
+
+	printf("%p %d %p\n", ptr3, *ptr3, &ptr3);
+
+	ptr1++;
+	ptr1--;
+	--ptr1;
+	++ptr1;
+
+	if (ptr1 == ptr3)
+	{
+		printf("Same\n");
+	}
+	else
+	{
+		printf("Different\n");
+	}
+
+
+	double d = 3.14;
+	double* ptrD = &d;
+
+	if (ptr1 == ptrD) // warning C4133
+	{
+		printf("Same\n");
+	}
+	else
+	{
+		printf("Different\n");
+	}
+
+	return 0;
+}
