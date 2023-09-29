1b1eccbc2cf5b435d8f2275477b6a242a74a0a02
seyeon040768 <seyeon040768@gmail.com>
2023-09-29 20:20:05 +0900

    update(code): Section12 Lecture6

---
+++ code/Section12/Lecture6/Lecture6.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+void Temp(register int r)
+{
+	// do something
+}
+
+int main(void)
+{
+	register int r;
+	r = 123;
+
+	int* ptr = &r; // Error C2103 '&' on register variable
+
+	return 0;
+}
