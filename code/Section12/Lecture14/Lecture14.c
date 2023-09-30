cf2aa12cd4b1057a50275eea75849c74218998ba
seyeon040768 <seyeon040768@gmail.com>
2023-09-30 20:36:40 +0900

    update(code): Section12 Lecture14

---
+++ code/Section12/Lecture14/Lecture14.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Dummy Output\n");
+
+	{
+		int n = 100000000;
+		int* ptr = (int*)malloc(n * sizeof(int));
+
+		if (!ptr)
+		{
+			printf("malloc() failed\n");
+			exit(EXIT_FAILURE);
+		}
+
+		for (int i = 0; i < n; ++i)
+		{
+			ptr[i] = i + 1;
+		}
+
+		free(ptr);
+	}
+
+	printf("Dummy Output\n");
+
+	return 0;
+}
