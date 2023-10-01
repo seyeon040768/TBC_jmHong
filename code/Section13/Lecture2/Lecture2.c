0f6b9b65b1c0cc94fcca384f4f84ff5e2a09cee1
seyeon040768 <seyeon040768@gmail.com>
2023-10-01 23:50:52 +0900

    update(code): Section13 Lecture2

---
+++ code/Section13/Lecture2/Lecture2.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int ch;
+	FILE* fr;
+	FILE* fw;
+
+	const char* outFilename = "output.txt";
+
+	unsigned long count = 0;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s filename\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((fr = fopen(argv[1], "r")) == NULL)
+	{
+		printf("Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((fw = fopen(outFilename, "w")) == NULL)
+	{
+		printf("Can't open %s\n", outFilename);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = fgetc(fr)) != EOF)
+	{
+		fputc(ch, stdout);
+
+		fputc(ch, fw);
+
+		++count;
+	}
+
+	fclose(fr);
+	fclose(fw);
+
+	printf("FILE %s has %lu characters\n", argv[1], count);
+
+	return 0;
+}
