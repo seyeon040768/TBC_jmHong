30c6f3dfb687a99573d6cc089750b1ac2eabe05c
seyeon040768 <seyeon040768@gmail.com>
2023-10-03 01:07:17 +0900

    update(code): Section13 Lecture8

---
+++ code/Section13/Lecture8/Lecture8.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main(void)
+{
+	FILE* fp = fopen("test.txt", "rb");
+
+	unsigned char ch;
+
+	SetConsoleOutputCP(CP_UTF8);
+
+	while (fread(&ch, sizeof(unsigned char), 1, fp) > 0)
+	{
+		printf("%hhu %c\n", ch, ch);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
