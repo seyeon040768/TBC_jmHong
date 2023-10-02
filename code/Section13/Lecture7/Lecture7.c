04fd3f5cba05e818e36beda664a530526f3c9fc4
seyeon040768 <seyeon040768@gmail.com>
2023-10-03 00:23:31 +0900

    update(code): Section13 Lecture7

---
+++ code/Section13/Lecture7/Lecture7.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp;
+	int ch;
+
+	fp = fopen("input.txt", "r");
+
+	ch = fgetc(fp);
+	fputc(ch, stdout); // "
+
+	ungetc(ch, fp);
+
+	ch = fgetc(fp);
+	fputc(ch, stdout); // "
+
+	fclose(fp);
+
+	printf("\n");
+
+
+	fp = fopen("input.txt", "r");
+
+	char buffer[32] = { '\0', };
+
+	setvbuf(fp, buffer, _IOFBF, sizeof(buffer));
+
+	ch = fgetc(fp);
+
+	for (int i = 0; i < sizeof(buffer); ++i)
+	{
+		printf("%c", buffer[i]); // "I choose a lazy person to do a
+	}
+	printf("\n");
+
+	fclose(fp);
+
+
+	// fflush(fp);
+
+	return 0;
+}
