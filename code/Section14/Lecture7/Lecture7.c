0a7b7fd148078c3593714b64ac6079afb5d38094
seyeon040768 <seyeon040768@gmail.com>
2023-10-05 23:28:51 +0900

    update(code): Section14 Lecture7

---
+++ code/Section14/Lecture7/Lecture7.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#define FUNDLEN 50
+
+struct Fortune
+{
+	char bankName[FUNDLEN];
+	double bankSaving;
+	char fundName[FUNDLEN];
+	double fundInvest;
+};
+
+double Sum(const struct Fortune* myFortune);
+
+int main(void)
+{
+	struct Fortune myFortune = {
+		"Wells-Fargo",
+		4032.27,
+		"JPMorgan Chase",
+		8543.94
+	};
+
+	printf("Total : $%.2f.\n",
+		Sum(&myFortune));
+
+	return 0;
+}
+
+double Sum(const struct Fortune *myFortune)
+{
+	return myFortune->bankSaving + myFortune->fundInvest;
+}
