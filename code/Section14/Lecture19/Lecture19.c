d0afa2f7586ad327d907ade6ea243a1b7b148dd5
seyeon040768 <seyeon040768@gmail.com>
2023-10-09 21:07:52 +0900

    update(code): Section14 Lecture19

---
+++ code/Section14/Lecture19/Lecture19.c
@@ -0,0 +1,79 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LEN 30
+
+enum Spectrum { red, orange, yellow, green, blue };
+const char* colors[] = { "red", "orange", "yellow", "green", "blue" };
+
+int main(void)
+{
+	char choice[LEN] = { 0, };
+	enum Spectrum color;
+	bool isColorFounded = false;
+
+	while (1)
+	{
+		bool isColorFounded = false;
+
+		printf("Input a color name (empty line to quit):\n");
+
+		if (scanf("%[^\n]%*c", choice) != 1)
+		{
+			break;
+		}
+
+		/*fgets(choice, LEN, stdin);
+
+		char* find = strchr(choice, '\n');
+		if (find)
+		{
+			*find = '\0';
+		}
+
+		if (choice[0] == '\0')
+		{
+			break;
+		}*/
+
+		for (color = red; color <= blue; ++color)
+		{
+			if (strcmp(choice, colors[color]) == 0)
+			{
+				isColorFounded = true;
+				break;
+			}
+		}
+
+		if (isColorFounded)
+		{
+			switch (color)
+			{
+			case red:
+				printf("Red roses\n");
+				break;
+			case orange:
+				printf("Oranges are delicious\n");
+				break;
+			case yellow:
+				printf("Yellow sunflowers\n");
+				break;
+			case green:
+				printf("Green apples\n");
+				break;
+			case blue:
+				printf("Blue ocean\n");
+				break;
+			}
+		}
+		else
+		{
+			printf("Please try different color %s.\n", choice);
+		}
+	}
+
+	printf("Good bye!\n");
+
+	return 0;
+}
