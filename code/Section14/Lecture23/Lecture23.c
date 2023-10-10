d07bbe5f6e279cebbd6a3c6378a4675e6e277147
Seyeon Lee <seyeon040768@gmail.com>
2023-10-10 23:52:38 +0900

    edit(code): Section14 Lecture23 add code

--- code/Section14/Lecture23/Lecture23.c
+++ code/Section14/Lecture23/Lecture23.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-int main(void)
+/*int main(void)
 {
 	typedef unsigned char BYTE;
 
@@ -43,6 +43,42 @@ int main(void)
 	Rect r1 = { 1.1, 2.2 };
 	Rect r2 = r1;
 	printf("%f %f\n", r2.width, r2.height);
+
+	return 0;
+}*/
+
+
+char char3[3] = { 'A', 'B', 'C' };
+
+char(*ComplicatedFunction1())[3] // Function Returns Pointer To Char[3]
+{
+	return &char3;
+}
+
+typedef char(*FRPTC3())[3]; // Function Returns Pointer To Char[3]
+typedef char(*(*PTFRPTC3)())[3];
+
+char(*(*fptr1)())[3] = ComplicatedFunction1;
+
+FRPTC3* fptr2 = ComplicatedFunction1;
+PTFRPTC3 fptr3 = ComplicatedFunction1;
+
+typedef char c3[3];
+
+c3* ComplicatedFunction2() // Function Returns Pointer To Char[3]
+{
+	return &char3;
+}
+
+int main(void)
+{
+	char(*retVal)[3] = fptr1();
+
+	printf("%c %c %c\n", (*retVal)[0], (*retVal)[1], (*retVal)[2]); // A B C
+
+	c3* myC3 = fptr2();
+
+	printf("%c %c %c\n", (*myC3)[0], (*myC3)[1], (*myC3)[2]); // A B C
 
 	return 0;
 }
