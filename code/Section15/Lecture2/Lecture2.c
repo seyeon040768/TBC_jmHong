6b4901d2de2fb8d3c1ea7340839233d0865461b5
seyeon040768 <seyeon040768@gmail.com>
2023-10-12 23:38:19 +0900

    update(code): Section15 Lecture2

---
+++ code/Section15/Lecture2/Lecture2.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+unsigned char ToDecimal(const char bi[]);
+
+int main(void)
+{
+	printf("Binary (string) to Decimal conversion\n");
+
+	printf("%d\n", ToDecimal("00000110"));
+	printf("%d\n", ToDecimal("00010110"));
+
+	printf("%d\n", ToDecimal("10010100"));
+
+	return 0;
+}
+
+unsigned char ToDecimal(const char bi[])
+{
+	unsigned char power = 1;
+	unsigned char num = 0;
+
+	for (size_t i = strlen(bi); i > 0; --i)
+	{
+		num += (bi[i - 1] - '0') * power;
+		power *= 2;
+	}
+
+	return num;
+}
