b62b0e8a36538d5f9050aab785aa534dfc3b50cf
seyeon040768 <seyeon040768@gmail.com>
2023-10-12 14:28:10 +0900

    update(code): Section15 Lecture1

---
+++ code/Section15/Lecture1/Lecture1.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	unsigned char a = 6; // 00000110
+	unsigned char b = 5; // 00000101
+
+	printf("%hhu\n", a & b); // 4 (a & b = 00000100)
+	printf("%hhu\n", a | b); // 7 (a | b = 00000111)
+	printf("%hhu\n", a ^ b); // 3 (a ^ b = 00000011)
+	printf("%hhu\n", ~a); // 249 (~a = 11111011)
+
+	return 0;
+}
