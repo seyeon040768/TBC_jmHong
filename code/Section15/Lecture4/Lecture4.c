7b7c4735e6d38f509934724a530e4032635ab2e7
seyeon040768 <seyeon040768@gmail.com>
2023-10-13 22:28:18 +0900

    update(code): Section15 Lecture4

---
+++ code/Section15/Lecture4/Lecture4.c
@@ -0,0 +1,68 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+unsigned char ToDecimal(const char bi[]);
+void PrintBinary(const unsigned char num);
+
+int main(void)
+{
+	unsigned char a = 6;
+	unsigned char b = 5;
+
+	printf("%hhu\n", a); // 6
+	PrintBinary(a); // Decimal 6       == Binary 00000110
+
+	printf("%hhu\n", b); // 5
+	PrintBinary(b); // Decimal 5       == Binary 00000101
+
+	printf("%hhu\n", a & b); // 4
+	PrintBinary(a & b); // Decimal 4       == Binary 00000100
+
+	printf("%hhu\n", a | b); // 3
+	PrintBinary(a | b); // Decimal 7       == Binary 00000111
+
+	printf("%hhu\n", a ^ b); // 2
+	PrintBinary(a ^ b); // Decimal 3       == Binary 00000011
+
+	printf("%hhu\n", ~a); // 1
+	PrintBinary(~a); // Decimal 249     == Binary 11111001
+
+	return 0;
+}
+
+unsigned char ToDecimal(const char bi[])
+{
+	unsigned char power = 1;
+	unsigned char num = 0;
+
+	for (size_t i = strlen(bi); i > 0; --i)
+	{
+		unsigned char temp = bi[i - 1] - '0';
+		if (temp != 0 && temp != 1)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		num += (bi[i - 1] - '0') * power;
+		power *= 2;
+	}
+
+	return num;
+}
+
+void PrintBinary(const unsigned char num)
+{
+	size_t bits = sizeof(num) * 8;
+	unsigned char bitmask = (unsigned char)pow(2, bits - 1);
+
+	printf("Decimal\t%d\t== Binary ", num);
+
+	for (size_t i = 0; i < bits; ++i)
+	{
+		printf("%d", (num & bitmask) == bitmask);
+		bitmask /= 2;
+	}
+	printf("\n");
+}
