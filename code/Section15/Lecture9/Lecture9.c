37cbbd280760a8b357c8e5a1dcb06debeb97d131
seyeon040768 <seyeon040768@gmail.com>
2023-10-14 23:50:53 +0900

    update(code): Section15 Lecture9

---
+++ code/Section15/Lecture9/Lecture9.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+struct
+{
+	bool hasSword : 1;
+	bool hasShield : 1;
+	bool hasPotion : 1;
+	bool hasGuntlet : 1;
+	bool hasHammer : 1;
+	bool hasKey : 1;
+	bool hasRing : 1;
+	bool hasAmulet : 1;
+} ItemsFlag;
+
+int main(void)
+{
+	ItemsFlag.hasSword = 1;
+
+	return 0;
+}
