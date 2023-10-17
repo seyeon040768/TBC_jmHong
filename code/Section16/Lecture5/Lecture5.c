891c9ba981bc80ee5e4051b99f879fe71ff3fead
Seyeon Lee <seyeon040768@gmail.com>
2023-10-17 23:21:24 +0900

    update(code): Section16 Lecture5

---
+++ code/Section16/Lecture5/Lecture5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PRINT(X, ...) printf("Message " #X ": " __VA_ARGS__)
+// ...: ellipses
+// __VA_ARGS: one of the predefined macro
+
+int main(void)
+{
+	double x = 48;
+	double y;
+
+	y = sqrt(x);
+
+	PRINT(1, "x = %g\n", x);
+	printf("Message " "1" ": " "x = %g\n", x);
+
+	PRINT(2, "x = %.2f, y = %.4f\n", x, y);
+	printf("Message " "2" ": " "x = %.2f, y = %.4f\n", x, y);
+
+	return 0;
+}
