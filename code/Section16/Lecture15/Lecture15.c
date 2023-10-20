0eb063f6dfa12ffede6a19c762f3a4dbbbdfa15f
Seyeon Lee <seyeon040768@gmail.com>
2023-10-20 23:57:16 +0900

    update(code): Section16 Lecture15

---
+++ code/Section16/Lecture15/Lecture15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+
+int Divide(int a, int b);
+
+int main(void)
+{
+	int a, b;
+	int f = scanf("%d %d", &a, &b);
+
+	printf("a / b = %d\n", Divide(a, b));
+
+	return 0;
+}
+
+int Divide(int a, int b)
+{
+	assert(b != 0);
+	//static_assert(1 == 0);
+
+	return a / b;
+}
