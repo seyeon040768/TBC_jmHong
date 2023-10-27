267720faeec5bb4df8c5bce76d2838c7e45a8c49
seyeon040768 <seyeon040768@gmail.com>
2023-10-27 23:08:05 +0900

    edit(code): Section17 Lecture1 use preprocessor to set array size

--- code/Section17/Lecture1/Lecture1.c
+++ code/Section17/Lecture1/Lecture1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TEST_SIZE TEST_SIZE
+
 #define PROMPT ">>"
 
 enum Menu
@@ -44,7 +46,7 @@ char** ChangeLengthMovieNames(char** movieNames, int moviesCount, int pivot, int
 	{
 		for (int i = 0; i < moviesCount; ++i)
 		{
-			if ((namesTemp[i] = (char*)malloc(100 * sizeof(char))) == NULL)
+			if ((namesTemp[i] = (char*)malloc(TEST_SIZE * sizeof(char))) == NULL)
 			{
 				exit(EXIT_FAILURE);
 			}
@@ -64,7 +66,7 @@ char** ChangeLengthMovieNames(char** movieNames, int moviesCount, int pivot, int
 			namesTemp[i] = movieNames[i - 1];
 		}
 
-		if ((namesTemp[pivot] = (char*)malloc(100 * sizeof(char))) == NULL)
+		if ((namesTemp[pivot] = (char*)malloc(TEST_SIZE * sizeof(char))) == NULL)
 		{
 			exit(EXIT_FAILURE);
 		}
@@ -129,13 +131,13 @@ float* ChangeLengthMovieStars(float* movieStars, int moviesCount, int pivot, int
 
 int main(void)
 {
-	char filename[100] = { '\0',};
+	char filename[TEST_SIZE] = { '\0',};
 	FILE* fp, *fSave;
 	int moviesCount, selectedMenu, selectedIndex;
 	char** movieNames;
 	float* movieStars;
-	char saveFilename[100] = { '\0', };
-	char movieNameForSearch[100] = { '\0', };
+	char saveFilename[TEST_SIZE] = { '\0', };
+	char movieNameForSearch[TEST_SIZE] = { '\0', };
 	int quit = 0, foundMovieByName = 0;
 
 	printf("Please input filename to read and press Enter.\n");
