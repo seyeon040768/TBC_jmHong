7b1d01b24767a95823625b56d12b4713c3fe89c5
seyeon040768 <seyeon040768@gmail.com>
2023-10-29 21:20:55 +0900

    update(code): Section17 Lecture4

---
+++ code/Section17/Lecture4/Lecture4.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+	int value;
+	struct Node* next;
+};
+
+void PrintAll(struct Node* head)
+{
+	printf("--------------------\n");
+	printf("Head address = %zd\n", (unsigned long long)head);
+	struct Node* search = head;
+	while (search != NULL)
+	{
+		printf("%zd %d %zd\n", (unsigned long long)search, search->value, (unsigned long long)search->next);
+		search = search->next;
+	}
+}
+
+int main(void)
+{
+	struct Node* head = NULL;
+
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	if (newNode == NULL)
+	{
+		printf("ERROR: malloc failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	newNode->value = 1;
+	newNode->next = NULL;
+
+	if (head == NULL)
+	{
+		head = newNode;
+	}
+
+	PrintAll(head);
+
+
+	newNode = (struct Node*)malloc(sizeof(struct Node));
+	if (newNode == NULL)
+	{
+		printf("ERROR: malloc failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newNode->value = 2;
+	newNode->next = NULL;
+
+	return 0;
+}
