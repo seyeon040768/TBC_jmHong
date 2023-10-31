b20081c1b1895f2fafb04b2b3ee1ef5535c2af70
Seyeon Lee <seyeon040768@gmail.com>
2023-10-31 23:27:14 +0900

    update(code): Section17 Lecture7

---
+++ code/Section17/Lecture7/Lecture7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define TEXT_SIZE 100
+
+/*struct Movie
+{
+	char title[TEXT_SIZE];
+	float rating;
+	struct Movie* next;
+};*/
+
+
+/*struct Movie
+{
+	char title[TEXT_SIZE];
+	float rating;
+};
+
+typedef struct Movie Item;
+
+typedef struct Node
+{
+	Item item;
+	struct Node* next;
+} Node;
+
+typedef struct List
+{
+	Node* head;
+	int size;
+} List;*/
+
+
+int main(void)
+{
+
+
+	return 0;
+}
