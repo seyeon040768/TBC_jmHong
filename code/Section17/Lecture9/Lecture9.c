00a0bc35dec73e12fdd1b9586b05dc2f6f3fa308
Seyeon Lee <seyeon040768@gmail.com>
2023-11-05 23:53:38 +0900

    update(code): Section17 Lecture9

---
+++ code/Section17/Lecture9/Lecture9.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "stack.h"
+
+PrintStack(const Stack* stack)
+{
+	printf("Stack: ");
+	if (IsEmpty(stack))
+	{
+		printf("Empty");
+	}
+	else
+	{
+		for (int i = 0; i <= stack->top; ++i)
+		{
+			printf("%d ", stack->items[i].key);
+		}
+	}
+	printf("\n");
+}
+
+Element GetElement(const int key)
+{
+	Element newElement;
+	newElement.key = key;
+	return newElement;
+}
+
+int main(void)
+{
+	Stack myStack;
+
+	Initialize(&myStack);
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(1));
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(3));
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(4));
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(5);
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(6));
+	PrintStack(&myStack);
+
+	Push(&myStack, GetElement(7));
+	PrintStack(&myStack);
+
+	for (int i = 0; i < MAX_STACK_SIZE + 1; ++i)
+	{
+		printf("Pop: %d\n", Pop(&myStack).key);
+		PrintStack(&myStack);
+	}
+
+	return 0;
+}
---
+++ code/Section17/Lecture9/stack.c
@@ -0,0 +1,22 @@
+#include "stack.h"
+
+void Initialize(Stack* stack)
+{
+
+}
+bool IsFull(const Stack* stack)
+{
+
+}
+bool IsEmpty(const Stack* stack)
+{
+
+}
+void Push(Stack* stack, Element item)
+{
+
+}
+Element Pop(Stack* stack)
+{
+
+}
