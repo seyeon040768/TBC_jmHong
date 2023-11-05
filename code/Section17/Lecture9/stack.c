2b57ddc38ec708648054b7a50cc43a773f7643e1
Seyeon Lee <seyeon040768@gmail.com>
2023-11-06 00:14:29 +0900

    update(code): Section17 Lecture9 add function
    IsFull

--- code/Section17/Lecture9/stack.c
+++ code/Section17/Lecture9/stack.c
@@ -4,18 +4,22 @@ void Initialize(Stack* stack)
 {
 	stack->top = -1;
 }
+
 bool IsFull(const Stack* stack)
 {
-
+	return stack->top + 1 >= MAX_STACK_SIZE;
 }
+
 bool IsEmpty(const Stack* stack)
 {
 
 }
+
 void Push(Stack* stack, Element item)
 {
 
 }
+
 Element Pop(Stack* stack)
 {
 
