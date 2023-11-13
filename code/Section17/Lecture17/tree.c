37e46631e25c93feb8427b49115498c4151c9de6
Seyeon Lee <seyeon040768@gmail.com>
2023-11-13 23:22:39 +0900

    update(code): Section17 Lecture17 add function
    AddItem, InTree

--- code/Section17/Lecture17/tree.c
+++ code/Section17/Lecture17/tree.c
@@ -5,6 +5,21 @@
 
 static int(*CompareFunc)(const Item item1, const Item item2) = NULL;
 
+Node* MakeNode(Item item)
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+	if (node == NULL)
+	{
+		return NULL;
+	}
+
+	node->item = item;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
 void Initialize(Tree* pTree, int(Compare)(const Item item1, const Item item2))
 {
 	pTree->root = NULL;
@@ -22,17 +37,85 @@ bool IsFull(const Tree* pTree)
 	return pTree->size == MAX_TREE_SIZE;
 }
 
-bool AddItem(const Item* pi, Tree* pTree)
+bool AddItem(const Item* pItem, Tree* pTree)
 {
+	if (IsEmpty(pTree))
+	{
+		if (!(pTree->root = MakeNode(*pItem)))
+		{
+			return false;
+		}
+		return true;
+	}
 
+	Node* search = pTree->root;
+
+	while (search != NULL)
+	{
+		if (pItem->key == search->item.key)
+		{
+			return false;
+		}
+
+		if (pItem->key < search->item.key)
+		{
+			if (search->left == NULL)
+			{
+				if (!(search->left = MakeNode(*pItem)))
+				{
+					return false;
+				}
+				return true;
+			}
+			search = search->left;
+		}
+		else
+		{
+			if (search->right == NULL)
+			{
+				if (!(search->right = MakeNode(*pItem)))
+				{
+					return false;
+				}
+				return true;
+			}
+			search = search->right;
+		}
+	}
+
+	return false;
 }
 
-bool InTree(const Item* pi, const Tree* pTree)
+bool InTree(const Item* pItem, const Tree* pTree)
 {
+	if (IsEmpty(pTree))
+	{
+		return false;
+	}
 
+	Node* search = pTree->root;
+
+	while (search != NULL)
+	{
+		if (pItem->key == search->item.key)
+		{
+			return true;
+		}
+
+		if (pItem->key < search->item.key)
+		{
+			search = search->left;
+		}
+		else
+		{
+			search = search->right;
+		}
+	}
+
+	return false;
 }
 
-bool DeleteItem(const Item* pi, Tree* pTree)
+bool DeleteItem(const Item* pItem, Tree* pTree)
 {
 
 }
